ecd80011ea97df0ac2b5b9b09873ab958318403d
Geoff Pitfield <geoff@pitfield.com>
2020-02-20 10:27:50 -0800

    revert test settings

--- timer.js
+++ timer.js
@@ -11,36 +11,35 @@ function tick() {
   var now = new Date;
   var remain = ((start - now) / 1000);
   var hh = parseInt((remain / 60 / 60));
-  // var mm = pad((remain / 60) % 60);
   var mm = parseInt((remain / 60) % 60);
   var ss = pad(remain % 60);
-  // if (hh > 0) {
-  //   document.getElementById('preposition').innerHTML = "at"
-  //   document.getElementById('time').innerHTML = "7pm"
-  //   document.getElementById('units').innerHTML = ""
-  // } else if (mm > 0) {
-  //   // hh + ":" + mm + ":" + ss;
-  //   // mm + ":" + ss;
-  //   document.getElementById('preposition').innerHTML = "in"
-  //   document.getElementById('time').innerHTML = mm
-  //   document.getElementById('units').innerHTML = "minutes"
-  //   setTimeout(tick, 60000);
-  // } else if (remain >= 11) {
-  //   document.getElementById('preposition').innerHTML = "in"
-  //   document.getElementById('time').innerHTML = ss
-  //   document.getElementById('units').innerHTML = "seconds"
-  //   setTimeout(tick, 1000);
-  // } else if (remain >= 1) {
-  //   var pregames = document.getElementsByClassName('pregame')
-  //   for (i = 0; i < pregames.length; i++) {
-  //     pregames[i].classList.add('is-hidden')
-  //   }
-  //   document.getElementById('countdown').classList.remove("is-hidden")
-  //   ss = remain % 60 // no padding
-  //   document.getElementById('countdown').innerHTML = Math.floor(ss)
-  //   setTimeout(tick, 1000);
-  // } else {
-  if (true) {
+  if (hh > 0) {
+    document.getElementById('preposition').innerHTML = "at"
+    document.getElementById('time').innerHTML = "7pm"
+    document.getElementById('units').innerHTML = ""
+  } else if (mm > 0) {
+    document.getElementById('preposition').innerHTML = "in"
+    document.getElementById('time').innerHTML = mm
+    document.getElementById('units').innerHTML = "minutes"
+    if (mm == 1) {
+      document.getElementById('units').innerHTML = "minute"
+    }
+    setTimeout(tick, 60000);
+  } else if (remain >= 11) {
+    document.getElementById('preposition').innerHTML = "in"
+    document.getElementById('time').innerHTML = ss
+    document.getElementById('units').innerHTML = "seconds"
+    setTimeout(tick, 1000);
+  } else if (remain >= 1) {
+    var pregames = document.getElementsByClassName('pregame')
+    for (i = 0; i < pregames.length; i++) {
+      pregames[i].classList.add('is-hidden')
+    }
+    document.getElementById('countdown').classList.remove("is-hidden")
+    ss = remain % 60 // no padding
+    document.getElementById('countdown').innerHTML = Math.floor(ss)
+    setTimeout(tick, 1000);
+  } else {
     var pregames = document.getElementsByClassName('pregame')
     for (i = 0; i < pregames.length; i++) {
       pregames[i].classList.add('is-hidden')
