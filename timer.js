9904683f6b6e072ed6d840aa4c4f0a808019f30b
Geoff Pitfield <geoff@pitfield.com>
2020-02-20 10:24:23 -0800

    testing player list redirect

--- timer.js
+++ timer.js
@@ -14,32 +14,33 @@ function tick() {
   // var mm = pad((remain / 60) % 60);
   var mm = parseInt((remain / 60) % 60);
   var ss = pad(remain % 60);
-  if (hh > 0) {
-    document.getElementById('preposition').innerHTML = "at"
-    document.getElementById('time').innerHTML = "7pm"
-    document.getElementById('units').innerHTML = ""
-  } else if (mm > 0) {
-    // hh + ":" + mm + ":" + ss;
-    // mm + ":" + ss;
-    document.getElementById('preposition').innerHTML = "in"
-    document.getElementById('time').innerHTML = mm
-    document.getElementById('units').innerHTML = "minutes"
-    setTimeout(tick, 60000);
-  } else if (remain >= 11) {
-    document.getElementById('preposition').innerHTML = "in"
-    document.getElementById('time').innerHTML = ss
-    document.getElementById('units').innerHTML = "seconds"
-    setTimeout(tick, 1000);
-  } else if (remain >= 1) {
-    var pregames = document.getElementsByClassName('pregame')
-    for (i = 0; i < pregames.length; i++) {
-      pregames[i].classList.add('is-hidden')
-    }
-    document.getElementById('countdown').classList.remove("is-hidden")
-    ss = remain % 60 // no padding
-    document.getElementById('countdown').innerHTML = Math.floor(ss)
-    setTimeout(tick, 1000);
-  } else {
+  // if (hh > 0) {
+  //   document.getElementById('preposition').innerHTML = "at"
+  //   document.getElementById('time').innerHTML = "7pm"
+  //   document.getElementById('units').innerHTML = ""
+  // } else if (mm > 0) {
+  //   // hh + ":" + mm + ":" + ss;
+  //   // mm + ":" + ss;
+  //   document.getElementById('preposition').innerHTML = "in"
+  //   document.getElementById('time').innerHTML = mm
+  //   document.getElementById('units').innerHTML = "minutes"
+  //   setTimeout(tick, 60000);
+  // } else if (remain >= 11) {
+  //   document.getElementById('preposition').innerHTML = "in"
+  //   document.getElementById('time').innerHTML = ss
+  //   document.getElementById('units').innerHTML = "seconds"
+  //   setTimeout(tick, 1000);
+  // } else if (remain >= 1) {
+  //   var pregames = document.getElementsByClassName('pregame')
+  //   for (i = 0; i < pregames.length; i++) {
+  //     pregames[i].classList.add('is-hidden')
+  //   }
+  //   document.getElementById('countdown').classList.remove("is-hidden")
+  //   ss = remain % 60 // no padding
+  //   document.getElementById('countdown').innerHTML = Math.floor(ss)
+  //   setTimeout(tick, 1000);
+  // } else {
+  if (true) {
     var pregames = document.getElementsByClassName('pregame')
     for (i = 0; i < pregames.length; i++) {
       pregames[i].classList.add('is-hidden')
@@ -54,6 +55,6 @@ function tick() {
 function loadAttend() {
   var urlParams = new URLSearchParams(window.location.search)
   if (urlParams.has('tournament_id')) {
-    window.location.href = `/attend.html?tournament_id=${urlParams.get('tournament_id')}`
+    window.location.href = `attend.html?tournament_id=${urlParams.get('tournament_id')}`
   }
 }
